c87d81a6367674b0c4150296f14930c138f093f4
vivianbarclay <vivian.m.barclay@gmail.com>
2025-02-18 05:52:31 +0000

    commit 3 - actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -14,4 +14,39 @@ class ActorsController < ApplicationController
       
     render({ :template => "actor_templates/show" })
   end
+
+  
+  def create
+    new_actor = Actor.new
+    new_actor.name = params[:name]
+    new_actor.dob = params[:dob]
+    new_actor.bio = params[:bio]
+    new_actor.image = params[:image]
+    new_actor.save
+
+    redirect_to("/actors")
+  end
+
+  def update
+    the_id = params.fetch("path_id")  # Define the_id properly
+    find_actor = Actor.where({:id => the_id})
+    update_actor = find_actor.at(0)
+
+    update_actor.name = params[:name]
+    update_actor.dob = params[:dob]
+    update_actor.bio = params[:bio]
+    update_actor.image = params[:image]
+    update_actor.save
+
+    redirect_to("/actors/#{the_id}")
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    actor = Actor.find(the_id)
+    actor.destroy
+  
+    redirect_to("/actors")
+  end
+
 end
